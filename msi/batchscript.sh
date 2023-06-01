23d7e3e9f529d3a7e2a3353d487d71cad02d6844
Saron Demeke <demek004@ln1002.mangi.msi.umn.edu>
2023-06-01 17:22:27 -0500

    check

--- msi/batchscript.sh
+++ msi/batchscript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --mem=1gb
+#SBATCH --ntasks=14
+#SBATCH --mem=10gb
 #SBATCH -t 00:00:10
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=demek004@umn.edu
 #SBATCH -p amdsmall
 cd ~/psy8960-week11/R
 module load R/4.2.2-openblas
-Rscript week11-cluster.R
+Rscript cluster_test.R
