ba281f0a35d59c84273ad70da331d8ca58d1230b
Saron Demeke <demek004@ln1001.mangi.msi.umn.edu>
2023-06-09 17:03:45 -0500

    trying 128 cores

--- msi/batchscript.sh
+++ msi/batchscript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks=64
-#SBATCH --mem=120gb
-#SBATCH -t 00:30:00
+#SBATCH --ntasks=128
+#SBATCH --mem=244gb
+#SBATCH -t 00:20:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=demek004@umn.edu
 #SBATCH -p amdsmall
